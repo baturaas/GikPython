3f39f6be4b79acc021a31c8c3de3c04299ff4ac7
Aleksey Batura <baturaas@gmail.com>
2019-10-10 13:43:52 +0300

    Clean commit + task3

--- task_3.py
+++ task_3.py
@@ -1,12 +1,12 @@
-#Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn. Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+# Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn. Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
 
 
-a = int(input("Введите целое число от 0 до 9: "))
-print(f"Вы ввели число - {a}")
+a = int(input("Enter an integer between 0 and 9: "))
+print(f"You entered a number: {a}")
 a = str(a)
 b = int(a + a)
 c = int(a + a + a)
 a = int(a)
-print(f"result -{a:>3},{b:>3},{c:>3}")
+print(f"Result: {a}, {b}, {c}")
 d = int(a + b + c)
-print(f"Сумма чисел{a:>3},{b:>3},{c:>3}равна:{d:<3}")
+print(f"Summ of numbers {a}, {b}, {c} is equal to:{d}")
