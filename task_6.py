0216140db2f3e49dbc37039ec4c49ac0d228bc9d
Aleksey Batura <baturaas@gmail.com>
2019-10-16 21:33:54 +0300

    New commit

--- task_6.py
+++ task_6.py
@@ -5,10 +5,10 @@ i = 1
 while True:
     print(f'Result per day {i},{daily_distance:.3f},{distance:.3f}')
     if distance < total_distance:
-        distance = distance + daily_distance
-        daily_distance = daily_distance * 1.1
-        i = i + 1
 
+        daily_distance = daily_distance * 1.1
+        distance = distance + daily_distance
+        i = i + 1
     else:
         print(f'Result will be achieved on {i} days')
         break
